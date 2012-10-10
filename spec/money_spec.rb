c6ff44fe17a7bd050b1af6140b7de49a3bc766fc
shio1997 <shio_ito@yahoo.co.jp>
2012-10-11 00:14:29 +0900

    4章完了

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,22 +3,28 @@
 require './dollar'
 
 describe Dollar do
-  describe "#dollar" do
-    context "乗法テスト" do
-      it 'X' do
-        five = Dollar.new(5)
-        product = five.times(2)
-        product.amount.should == 10
+  describe "#times" do
+    let(:five) {Dollar.new(5)}
 
-        product = five.times(3)
-        product.amount.should == 15
+    context "５に数値をかける場合" do
+      it '5X2=10' do
+        Dollar.new(10).equals(five.times(2)).should == true
+      end
+      it '5X3=15' do
+        Dollar.new(15).equals(five.times(3)).should == true
       end
     end
+  end
 
-    context "等価性テスト" do
-      it '=' do
-        Dollar.new(5).equals(Dollar.new(5)).should == true
-        Dollar.new(5).equals(Dollar.new(6)).should == false
+  describe "#equals" do
+    let(:five) {Dollar.new(5)}
+
+    context "５と比較する場合" do
+      it '5==5はtrue' do
+        five.equals(Dollar.new(5)).should == true
+      end
+      it '5==6はfalse' do
+        five.equals(Dollar.new(6)).should == false
       end
     end
 
